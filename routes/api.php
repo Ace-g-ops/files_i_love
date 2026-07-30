17ddd61b0346f4ef39db3051620777c4e7988252
Victor <victorajibua14@gmail.com>
2026-07-30 20:15:00 +0100

    modified routes, controllers and devlogs files

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,28 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/convert', [App\Http\Controllers\ConversionController::class, 'store']);
-Route::post('/convert-media', [App\Http\Controllers\MediaController::class, 'store']);
+//CONVERSION ROUTES
+Route::middleware('throttle:5,1')->group(function(){
+
+    Route::post('/convert', [App\Http\Controllers\ConversionController::class, 'store']);
+    Route::post('/convert-media', [App\Http\Controllers\MediaController::class, 'store']);
+
+});
+
+//STATUS ROUTES
+Route::middleware('throttle:60,1')->group(function(){
+
+    Route::get('/status/{id}', [App\Http\Controllers\ConversionController::class, 'status']); 
+    Route::get('/status/media/{id}',  [App\Http\Controllers\MediaController::class, 'status']);
+});
+
+//DOWNLOAD ROUTES
 Route::get('/download/{type}/{id}', [App\Http\Controllers\DownloadController::class, 'show']);
-Route::get('/status/{id}', [App\Http\Controllers\ConversionController::class, 'status']); 
-Route::get('/status/media/{id}',  [App\Http\Controllers\MediaController::class, 'status']);
+
+//ALPINE-FORMAT-CONFIG ROUTE
+Route::get('/format', function(){
+
+    return response()->json(config('Conversions'));
+});
+
+
